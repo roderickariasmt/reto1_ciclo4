25b0d6fac2656b03464ccb29efc52c453698c8c0
Roderick Santiago Arias <roderick.arias.mt@correo.usa.edu.co>
2021-12-19 19:05:22 -0500

    Reto 1

---
+++ src/main/java/com/usa/ciclo4/reto1_ciclo4/model/User.java
@@ -0,0 +1,22 @@
+package com.usa.ciclo4.reto1_ciclo4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+    private String name;
+
+}
